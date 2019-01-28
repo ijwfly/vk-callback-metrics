84d3be003bb0c81ab1dbaf399df237998c5c5dc9
Maxim Nechaev <maxnecha@gmail.com>
2019-01-28 13:00:42 +0300

    Rework event handling, automate counter creation

--- metrics.go
+++ metrics.go
@@ -14,18 +14,54 @@ import (
 
 var confirmationString string
 
-var vkMessageNew = promauto.NewCounter(prometheus.CounterOpts{
-	Name: "vk_message_new",
-})
-var vkWallReplyNew = promauto.NewCounter(prometheus.CounterOpts{
-	Name: "vk_wall_reply_new",
-})
-var vkGroupLeave = promauto.NewCounter(prometheus.CounterOpts{
-	Name: "vk_group_leave",
-})
-var vkGroupJoin = promauto.NewCounter(prometheus.CounterOpts{
-	Name: "vk_group_join",
-})
+var counterMap = make(map[string]prometheus.Counter)
+var eventTypes = []string{
+	"message_new",
+	"message_reply",
+	"message_edit",
+	"message_allow",
+	"message_deny",
+	"photo_new",
+	"photo_comment_new",
+	"photo_comment_restore",
+	"photo_comment_delete",
+	"audio_new",
+	"view_new",
+	"video_comment_new",
+	"video_comment_edit",
+	"video_comment_restore",
+	"video_comment_delete",
+	"wall_post_new",
+	"wall_repost",
+	"wall_reply_new",
+	"wall_reply_edit",
+	"wall_reply_restore",
+	"wall_reply_delete",
+	"board_post_new",
+	"board_post_edit",
+	"board_post_restore",
+	"board_post_delete",
+	"market_comment_new",
+	"market_comment_edit",
+	"market_comment_restore",
+	"market_comment_delete",
+	"group_leave",
+	"group_join",
+	"user_block",
+	"user_unblock",
+	"poll_vote_new",
+	"group_officers_edit",
+	"group_change_settings",
+	"group_change_photo",
+}
+
+func initializeHandlers() {
+	for _, eventType := range eventTypes {
+		counterMap[eventType] = promauto.NewCounter(prometheus.CounterOpts{
+			Name: eventType,
+		})
+	}
+}
 
 func callbackHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	var body []byte
@@ -42,24 +78,15 @@ func callbackHandler(responseWriter http.ResponseWriter, request *http.Request) 
 		return
 	}
 
-	switch requestType := parsedBody["type"]; requestType {
-	case "confirmation":
-		_, _ = fmt.Fprint(responseWriter, confirmationString)
-		return
-	case "message_new":
-		vkMessageNew.Add(1)
-	case "wall_reply_new":
-		vkWallReplyNew.Add(1)
-	case "group_leave":
-		vkGroupLeave.Add(1)
-	case "group_join":
-		vkGroupJoin.Add(1)
+	requestType := parsedBody["type"].(string)
+	if handler, ok := counterMap[requestType]; ok {
+		handler.Add(1)
 	}
-
 	_, _ = fmt.Fprint(responseWriter, "ok")
 }
 
 func main() {
+	initializeHandlers()
 	confirmationString = os.Getenv("CONFIRMATION_STR")
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/callback", callbackHandler)
